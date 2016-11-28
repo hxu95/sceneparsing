f710b0494901eb7ff189c20838e60a07eb003481
hxu <to.helen.xu@gmail.com>
2016-11-27 21:07:44 -0500

    updates on prediction

--- models/predict.py
+++ models/predict.py
@@ -9,17 +9,19 @@
 
 # Set python path here
 import sys
+from PIL import Image
 #manual path definition
-#sys.path.append('/home/hxu/caffe/python')
+sys.path.append('/home/hxu/caffe_main/python')
+
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 import caffe
+import Image
 import os
 
-base_dir = 'home/hxu/sceneparsing'
+base_dir = '/home/hxu/sceneparsing'
 models_dir = os.path.join(base_dir, 'models')
-print models_dir
 #sys.path.append('/home/hxu/caffe/python')
 
 # define model and weights
@@ -28,18 +30,19 @@ if (model_type is 'FCN'):
   model_definition = 'deploy_FCN.prototxt'
   model_weights = os.path.join(models_dir, 'FCN_iter_160000.caffemodel')
 elif (model_type is 'Dilated'):
-  model_definition = os.path.join(models_dir, 'deploy_DilatedNet.prototxt')
+  model_definition = 'deploy_DilatedNet.prototxt'
   model_weights = os.path.join(models_dir, 'DilatedNet_iter_120000.caffemodel')
 
 # output folder
 prediction_folder = os.path.join(base_dir, 'sampleData/predictions_' + model_type)
-print prediction_folder
 
 if not os.path.exists(prediction_folder):
     os.makedirs(prediction_folder)
 
 #load the net
-net = caffe.Net(model_definition, model_weights, caffe.TEST);
+net = caffe.Net(model_definition, model_weights, caffe.TEST)
+# net = caffe.Net('conv.prototxt', caffe.TEST)
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
 
 # load input images that we want to do segmentation on
 inputs = os.path.join(base_dir, 'sampleData/intermediate')
@@ -48,7 +51,40 @@ print inputs
 # loop through input images
 for filename in os.listdir(inputs):
   print filename
-  img = cv2.imread(os.path.join(inputs, filename))
-  out = net.forward(img)
+  # img = cv2.imread()
+  
+  # img = img.astype(np.float32)/255.
+  # img = img[...,::-1]
+  path = os.path.join(inputs, filename)
+  # img = cv2.imread(path)
+  img = caffe.io.load_image(path)
+  '''
+  # im = np.array(Image.open(os.path.join(inputs, filename)))
+  im_input = img[np.newaxis, np.newaxis, :, :]
+  net.blobs['data'].reshape(*im_input.shape)
+  # net.blobs['data'].data[...] = transformer.preprocess('data', img)
+  # out = net.forward()
+  net.blobs['data'].data[...] = im_input
+  # out = net.forward([img])
+  '''
+
+  # http://stackoverflow.com/questions/29124840/prediction-in-caffe-exception-input-blob-arguments-do-not-match-net-inputs
+  #net.blobs['data'].data[...] = transformer.preprocess('data', caffe.io.load_image(path))
+
+  # Pad an image so that it has a shape of a multiple of 32
+  # Reshape and fill input with the image
+  net.blobs['data'].reshape(1, 3, *img.shape[:2])
+  net.blobs['data'].data[0, ...] = np.rollaxis(img, 2)
+  # Predict and get the outputs albedo and shading
+  out = net.forward()
+
+  print out
+
+  a = out['upscore8'].copy()
+
   outfile = os.path.join(prediction_folder, filename)
-  cv2.imwrite(outfile, out)
+  plt.figure()
+  plt.imshow(a)
+  plt.show()
+  # cv2.imwrite(outfile, out)
+  plt.imsave(outfile, a)
